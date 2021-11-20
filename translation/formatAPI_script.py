27fba74e64539835d149c7db008807d23844a5e4
Dan Lee <danlee1025@gmail.com>
2021-11-19 19:10:28 -0500

    Parse for anchor tags.

--- translation/formatAPI_script.py
+++ translation/formatAPI_script.py
@@ -1,15 +1,13 @@
 from docx import Document
-import json 
+import json
 import os
 import re
 
-
-
-#Steps to run this code: 
+# Steps to run this code: 
     # 1. import docx and json using (pip)or pip3 install python-docx
-    # 2. run python script.py and it will output a data.json file 
-    # 3. right now the configuration is that the last 4 tables are not being used and if they ever want to be used, just add the header to the key and get rid of a pop() statement corresponding to that key
-    # 4. **IMPORTANT** make sure that the language and en variables in line 40-41 match up to table names
+    # 2. Create a directory called translate and put all the word docs that you want formatted
+    # 3. run python3 formatAPI_script.py
+    # 4. files will be output for each doc
 
 path = './translate'
 folder = os.fsencode(path)
@@ -21,19 +19,14 @@ for file in os.listdir(folder):
     if document.endswith( ('.docx') ):
         documents.append(document)
 
-# print(documents)
-
-
-for z in documents: 
-
+for z in documents:
     document = Document('./translate/' + str(z))
-
 
     initial_dict = {}
     #if ever you need to add mroe information to the headers and tables just update this
     headers_key = ["header", "vaccinepage", "dashboardpage", "vaccineform", "qrpage", "receivedpage", "footer", "FormatSms", "FormatNotFoundSms", "FormatHTML", "FormatNotFoundHTML"]
 
-    for i in range(len(document.tables)): 
+    for i in range(len(document.tables)):
         table = document.tables[i]
         keys = None
         data = []
@@ -70,25 +63,36 @@ for z in documents:
     #formatHTML
     headinginfo_format = overall_dict.get('FormatHTML').get('heading')
     infoText_format = overall_dict.get('FormatHTML').get('infoText').replace('24', '{linkExpireHours}')
+
     viewLink_format = overall_dict.get('FormatHTML').get('viewlink') or overall_dict.get('FormatHTML').get('mmatlink')
+    viewLink_format = viewLink_format.replace('<a>', '<a href=\'{url}\'>')
+
     learnMore_format = overall_dict.get('FormatHTML').get('learnMore')
+    learnMore_format = learnMore_format.replace('<a>', '<a href=\'{cdcUrl}\'>')
+
     questions_format = overall_dict.get('FormatHTML').get('questions')
+
     visitFAQ_format = overall_dict.get('FormatHTML').get('visitFAQ')
+    visitFAQ_format = visitFAQ_format.replace('<a>', '<a href=\'{vaccineFAQUrl}\'>')
+
     stayInformed_format = overall_dict.get('FormatHTML').get('stayInformed')
+
     viewInfo_format = overall_dict.get('FormatHTML').get('viewInfo') or overall_dict.get('FormatHTML').get('viewnfo')
+    viewInfo_format = viewInfo_format.replace('<a>', '<a href=\'{covidWebUrl}\'>')
+
     emailLabel_format = overall_dict.get('FormatHTML').get('emailLabel')
 
     output_str = '----------------------FORMATHTML----------------------------- \n\n\n'
 
     output_str += '$"<img src=\'{webUrl}/imgs/MyTurn-logo.png\'><br/>" +\n'
     output_str += '$"<h3 style=\'color: #f06724\'>' + headinginfo_format + '</h3>" +\n'
-    output_str +=  '$"<p>' + infoText_format + '</p> +\n'
-    output_str += '$"<p><a href=\'{url}\'>' + viewLink_format + '</a></p>" +\n'
-    output_str += '$"<p><a href=\'{cdcUrl}\'>' + learnMore_format + '</a></p>" +\n'
+    output_str +=  '$"<p>' + infoText_format + '</p>" +\n'
+    output_str += '$"<p><a href=\'{url}\'>' + viewLink_format + '</a></p>" +\n' if not '</a>' in viewLink_format else '$"<p>' + viewLink_format + '</p>" +\n'
+    output_str += '$"<p>' + learnMore_format + '</p>" +\n'
     output_str += '$"<p><b>' + questions_format + '</b></p>" +\n'
-    output_str += '$"<p><a href=\'{vaccineFAQUrl}\'>' + visitFAQ_format + '</a></p>" +\n'
+    output_str += '$"<p>' + visitFAQ_format + '</p>" +\n'
     output_str += '$"<p><b>' + stayInformed_format + '</b></p>" +\n'
-    output_str += '$"<p><a href=\'{covidWebUrl}\'>' + viewInfo_format + '</a></p><br/>" +\n'
+    output_str += '$"<p>' + viewInfo_format + '</p><br/>" +\n'
     output_str += '$"<hr>" +\n'
     output_str += '$"<footer><p style=\'text-align:center\'>' + emailLabel_format + '</p>" +\n'
     output_str += '$"<p style=\'text-align:center\'><img src=\'{emailLogoUrl}\'></p></footer>",'
@@ -99,23 +103,34 @@ for z in documents:
     output_str += '\n ----------------------------------FormatNotFoundHTML-------------------\n\n\n'
 
     headinginfo_noformat = overall_dict.get('FormatNotFoundHTML').get('heading')
-    infoText_noformat = overall_dict.get('FormatNotFoundHTML').get('infoText').replace('24', '{linkExpireHours}')
-    nextSteps_noformat = overall_dict.get('FormatNotFoundHTML').get('nextSteps')
-    questions_noformat = overall_dict.get('FormatNotFoundHTML').get('questions')
-    visitFAQ_noformat = overall_dict.get('FormatNotFoundHTML').get('visitFAQ')
-    stayInformed_noformat = overall_dict.get('FormatNotFoundHTML').get('stayInformed')
-    viewInfo_noformat = overall_dict.get('FormatNotFoundHTML').get('viewInfo') or overall_dict.get('FormatNotFoundHTML').get('mmatInfo')
-    emailLabel_noformat = overall_dict.get('FormatNotFoundHTML').get('emailLabel')
 
+    infoText_noformat = overall_dict.get('FormatNotFoundHTML').get('infoText')
+    infoText_noformat = infoText_noformat.replace('<a>', '<a href=\'{webUrl}\'>')
+
+    nextSteps_noformat = overall_dict.get('FormatNotFoundHTML').get('nextSteps')
+    nextSteps_noformat = nextSteps_noformat.replace('<a>', '<a href=\'{webUrl}\'>', 1)
+    nextSteps_noformat = nextSteps_noformat.replace('<a>', '<a href=\'{contactUsUrl}\'>')
+
+    questions_noformat = overall_dict.get('FormatNotFoundHTML').get('questions')
+
+    visitFAQ_noformat = overall_dict.get('FormatNotFoundHTML').get('visitFAQ')
+    visitFAQ_noformat = visitFAQ_noformat.replace('<a>', '<a href=\'{vaccineFAQUrl}\'>').replace('\n', '')
+
+    stayInformed_noformat = overall_dict.get('FormatNotFoundHTML').get('stayInformed')
+
+    viewInfo_noformat = overall_dict.get('FormatNotFoundHTML').get('viewInfo') or overall_dict.get('FormatNotFoundHTML').get('mmatInfo')
+    viewInfo_noformat = viewInfo_noformat.replace('<a>', '<a href=\'{covidWebUrl}\'>')
+
+    emailLabel_noformat = overall_dict.get('FormatNotFoundHTML').get('emailLabel')
 
     HTMLnotfound_str = '$"<img src=\'{webUrl}/imgs/MyTurn-logo.png\'><br/>" +\n'
     HTMLnotfound_str += '$"<h3 style=\'color: #f06724\'>' + headinginfo_noformat + '</h3>" +\n'
-    HTMLnotfound_str +=  '$"<p><a href=\'{webUrl}\'>' + infoText_noformat + '</a></p><br/> +\n'
-    HTMLnotfound_str += '$"<p><a href=\'{webUrl}\'><a href=\'{contactUsUrl}\'>' + nextSteps_noformat + '</a></a></p>" +\n'
+    HTMLnotfound_str +=  '$"<p>' + infoText_noformat + '</p><br/>" +\n'
+    HTMLnotfound_str += '$"<p>' + nextSteps_noformat + '</p>" +\n'
     HTMLnotfound_str += '$"<p><b>' + questions_noformat + '</b></p>" +\n'
-    HTMLnotfound_str += '$"<p> <a href=\'{vaccineFAQUrl}\' ' + visitFAQ_noformat + '</a> </p>" +\n'
+    HTMLnotfound_str += '$"<p>' + visitFAQ_noformat + '</p>" +\n'
     HTMLnotfound_str += '$"<p><b>' + stayInformed_noformat + '</b></p>" +\n'
-    HTMLnotfound_str += '$"<p><a href=\'{covidWebUrl}\'>' + viewInfo_noformat + '</a></p><br/>" +\n'
+    HTMLnotfound_str += '$"<p>' + viewInfo_noformat + '</p><br/>" +\n'
     HTMLnotfound_str += '$"<hr>" +\n'
     HTMLnotfound_str += '$"<footer><p style=\'text-align:center\'>' + emailLabel_noformat + '</p>" +\n'
     HTMLnotfound_str += '$"<p style=\'text-align:center\'><img src=\'{emailLogoUrl}\'></p></footer>",'
